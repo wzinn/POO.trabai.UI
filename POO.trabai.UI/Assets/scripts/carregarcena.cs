847fa0864ad6c9b17054dd6037a81b67ac728a9e
wzinn <ws91443973@gmail.com>
2025-08-11 10:24:26 -0300

    começo.POO.ui
    30 min

---
+++ POO.trabai.UI/Assets/scripts/carregarcena.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class carregarcena : MonoBehaviour
+{
+    public string nomeDaCena;
+
+    public void Carregar()
+    {
+        SceneManager.LoadScene(nomeDaCena);
+    }
+    
+    
+}
